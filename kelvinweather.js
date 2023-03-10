8eb2a6bb1720cc39bf7d35357e58b2b65fd272ab
d-brightmind <robiuridwanullah123@gmail.com>
2023-03-10 23:07:16 +0100

    adding kelvin weather file

--- kelvinweather.js
+++ kelvinweather.js
@@ -4,10 +4,10 @@ const kelvin = 293;
 const celsius = kelvin - 273;
 // Declaring Fahrenheit variable
 let fahrenheit =celsius * (9/5) + 32;
-// Rouding up fahrenheit value
+// Rounding up fahrenheit value
 fahrenheit = Math.floor(fahrenheit);
 // Print the result
-console.log(`The temperatue is ${fahrenheit} degrees Fahrenheit.`);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 // Declaring Newton variable
 let Newton = celsius * (33/100);
 Newton = Math.floor(Newton);
